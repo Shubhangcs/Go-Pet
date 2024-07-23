fe851b81826e536de6ff66f0e7a2c598e4ee557e
Shubhangcs <cshubhanga@gmail.com>
2024-07-24 00:52:45 +0530

    no issue

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 
 func main(){
-	db , err := sql.Open("postgres" , "host=localhost port=5432 user=root dbname=pet sslmode=disable password=password")
+	// db , err := sql.Open("postgres" , "host=localhost port=5432 user=root dbname=pet sslmode=disable password=password")
+	db , err := sql.Open("postgres" , "postgresql://root:xuc87BFMfwzq2xqFJHXf3I1AfNbwwTOz@dpg-cqg05256l47c73bn7gig-a.oregon-postgres.render.com/pet_69y5")
 	if err != nil{
 		panic(err.Error())
 	}
